8f2788886c46dde4e7e73da425632290851bed7d
kusuma <d.kusuma@tu-braunschweig.de>
2023-04-24 22:55:10 +0200

    only backward freezing considered

--- models/rec_decoders/deeplab_rec/config.py
+++ models/rec_decoders/deeplab_rec/config.py
@@ -15,20 +15,4 @@ decoder_layers = {
         4: ['last_conv.1', 'last_conv.0', 'aspp', 'conv1', 'bn1'],
         'propagation': 'Freezing propagates backwards.'
     },
-    'new_bw':{    
-        0: None,
-        1: ['last_conv.1'],
-        2: ['last_conv.1', 'last_conv.0'],
-        3: ['last_conv.1', 'last_conv.0', 'conv1', 'bn1'],
-        4: ['last_conv.1', 'last_conv.0', 'conv1', 'bn1', 'aspp'],
-        'propagation': 'Freezing propagates backwards.'
-    },
-    'lat_bw':{    
-        0: None,
-        1: ['aspp'],
-        2: ['aspp', 'last_conv.1'],
-        3: ['aspp', 'last_conv.1', 'last_conv.0'],
-        4: ['aspp', 'last_conv.1', 'last_conv.0', 'conv1', 'bn1'],
-        'propagation': 'Lateral backward freezing.'
-    },
 }
